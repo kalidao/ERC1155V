b418d68bea4948cac6ccd73b55a24b94a09d1fb7
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-09-19 14:12:54 +0900

    Update ERC1155.sol
    ⚖️ MIT

--- src/ERC1155.sol
+++ src/ERC1155.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: AGPL-3.0-only
+// SPDX-License-Identifier: MIT
 pragma solidity >=0.8.4;
 
 /// @notice A generic interface for a contract which properly accepts ERC-1155 tokens.
