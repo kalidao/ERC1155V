c8be5ee1fbe99a0e06e507498be3eb0bcc4ed3d2
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-09-29 14:50:40 +0900

    Update Deploy.s.sol
    🥢

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.4;
 
 import {Script} from "forge-std/Script.sol";
 
-import {ERC1155VToken } from "src/ERC1155VToken .sol";
+import {ERC1155VToken} from "src/ERC1155VToken.sol";
 
 /// @notice A very simple deployment script
 contract Deploy is Script {
@@ -12,7 +12,7 @@ contract Deploy is Script {
   /// @return greeter The deployed contract
   function run() external returns (ERC1155VToken vToken) {
     vm.startBroadcast();
-    vToken = new ERC1155VToken ();
+    vToken = new ERC1155VToken();
     vm.stopBroadcast();
   }
 }
