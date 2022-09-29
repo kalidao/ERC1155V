5d99f24767c3d905a19db518d57523fc13eed879
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-09-29 14:42:52 +0900

    Update ERC1155V.t.sol
    🥢

--- test/ERC1155V.t.sol
+++ test/ERC1155V.t.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.4;
 
 import "forge-std/Test.sol";
 
-import {ERC1155VToken } from "src/ERC1155VToken .sol";
+import {ERC1155VToken} from "src/ERC1155VToken.sol";
 
 contract ERC1155VTest is Test {
     using stdStorage for StdStorage;
